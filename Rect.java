af5048a860d621b5ade707a65a8570e79c4f017a
Jared Worthington <jworthington@email.arizona.edu>
2015-10-23 23:22:15 -0400

    netPaint commit

---
+++ Rect.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.awt.Color;
+import java.awt.Point;
+
+public class Rect extends PaintObj {
+
+	public Rect(Point a, int x, int y, Color c, String s) {
+		super(a, x, y, c, s);
+		// TODO Auto-generated constructor stub
+	}
+
+}
